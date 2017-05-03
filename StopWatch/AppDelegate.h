2c75d95158d33018fea722b3c6fd3982e880dbc2
黄伟祥 <vison@visondemacbook-pro.local>
2017-05-03 15:03:56 +0800

    Initial Commit

---
+++ StopWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StopWatch
+//
+//  Created by vison on 2017/5/3.
+//  Copyright © 2017年 vison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StopWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StopWatch
+//
+//  Created by vison on 2017/5/3.
+//  Copyright © 2017年 vison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
