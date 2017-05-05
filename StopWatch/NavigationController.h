3a90cd2b3f37ed4195b7a4de9c31806634e10e1d
黄伟祥 <vison@visondeMacBook-Pro.local>
2017-05-05 14:34:50 +0800

    first commit

---
+++ StopWatch/NavigationController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationController.h
+//  StopWatch
+//
+//  Created by vison on 2017/5/3.
+//  Copyright © 2017年 vison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationController : UINavigationController
+
+@end
